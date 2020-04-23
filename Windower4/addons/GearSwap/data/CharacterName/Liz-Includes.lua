195933312c98c16ff4ca94dfd2ccdd1d85b24241
Vernetzt <8786961+Vernetzt@users.noreply.github.com>
2020-04-23 08:47:41 -0600

    Delete Liz-Includes.lua

--- Windower4/addons/GearSwap/data/CharacterName/Liz-Includes.lua
+++
@@ -1,9 +0,0 @@
-version = "3.0"
-
-res = require('resources')
-texts = require('texts')
-
-include('Modes.lua')
-include('Liz-HelperFunctions.lua')
-include('Liz-Mappings.lua')
-include('Liz-HUD.lua')
